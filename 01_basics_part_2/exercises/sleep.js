2142b003ff96c7817926f24ecf540216e7c9f0d4
DasneiN <sank.mail@gmail.com>
2019-03-30 19:15:53 +0300

    implement sleep() function

--- 01_basics_part_2/exercises/sleep.js
+++ 01_basics_part_2/exercises/sleep.js
@@ -5,6 +5,13 @@
  * выполнение и вернуть undefined.
  */
 
-export default function sleep() {
-  // your code here
+export default function sleep(sec) {
+  const s = +sec;
+
+  if (!isNaN(s) && typeof s === 'number' && s > 0 && s % 1 === 0) {
+    const currentTime = new Date().getTime();
+    while (currentTime + +sec * 1000 >= new Date().getTime()) { }
+  } else {
+    void 0;
+  }
 }
